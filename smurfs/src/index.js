c7f81f1724422739a1fe2c287d536e87db698f14
eralpkor <eralp.ny@gmail.com>
2019-09-13 11:02:10 -0400

    answered Self-Study/Essay Questions

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+// import { reducer } from "./reducers";
+
+// const store = createStore(reducer, applyMiddleware(thunk));
+
 ReactDOM.render(<App />, document.getElementById("root"));
